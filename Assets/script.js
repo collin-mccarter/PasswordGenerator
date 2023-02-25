1bfe60169d25b426d3bd0afacf15175831d3b66e
collin-mccarter <collinmccarter12@gmail.com>
2023-02-25 16:35:14 -0600

    password gets generated, but it repeats itself

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordLength = 8;
-var lowercaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var uppercaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbersArray = ['0','1','2','3','4','5','6','7','8','9'];
-var specialCharactersArray = ['!','"','#','$','%','&',"'",'(',')','*','+','-','.','/',':',';','<','=',">",'?','@','^','_','`','{','|','}','~'];
+var lowercaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; // lowercase values
+var uppercaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; // uppercase values
+var numbersArray = ['0','1','2','3','4','5','6','7','8','9']; // all possible numbers
+var specialCharactersArray = ['!','"','#','$','%','&',"'",'(',')','*','+','-','.','/',':',';','<','=',">",'?','@','^','_','`','{','|','}','~']; // all special characters
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // on button click -> execute writePassword
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword(); // ? -> executes generatePassword function
+  var passwordText = document.querySelector("#password"); // adds passwordText in the body under id = password
 
   passwordText.value = password; // displays password on the screen
 
@@ -21,45 +20,50 @@ function writePassword() {
 
 // created generate password function
 function generatePassword() {
-  var choiceArray = [];
+  var choiceArray = []; // setting up blank array for data to be added to
 
+  // first prompt to determine password length
   passwordLength = parseInt(prompt("How many characters do you want the password to be? (At least 8, no more than 128 characters)")); {
+    // making sure password length is a reasonable number/length
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       alert("Password must be at least 8 characters and no more than 128 characters");
       return false;
     }
    }
    
+   // asking if lc characters should be included in password
    let passwordLowercase = confirm("Would you like lowercase characters included in your password?"); {
-    if (passwordLowercase) {
-      // console.log("yes lc") // if they click ok
-      console.log(choiceArray = choiceArray.concat(lowercaseArray));
+    if (passwordLowercase) { // if yes is clicked
+      console.log(choiceArray = choiceArray.concat(lowercaseArray)); // visually see lowercase characters added to modified choiceArray
     }
    }
    
+   // asks if you want to add uppercase characters
    let passwordUppercase = confirm("Would you like uppercase characters to be included in your password?"); {
-    if (passwordUppercase) {
-      console.log(choiceArray = choiceArray.concat(uppercaseArray));
+    if (passwordUppercase) { // if yes 
+      console.log(choiceArray = choiceArray.concat(uppercaseArray)); // adds uppercase to the choiceArray
     } 
    }
 
+   // asks if you want to add numbers to password
    let passwordNumbers = confirm("Would you like numbers to be included in your password?"); {
-    if (passwordNumbers) {
-      console.log(choiceArray = choiceArray.concat(numbersArray));
+    if (passwordNumbers) { // if yes
+      console.log(choiceArray = choiceArray.concat(numbersArray)); // adds numbers to choice array
     }
    }
 
+   // asks if you want to add special characters to the password
    let passwordSpecialCharacters = confirm("Would you like special characters to be included in your password?"); {
-    if (passwordSpecialCharacters) {
-      console.log(choiceArray = choiceArray.concat(specialCharactersArray));
+    if (passwordSpecialCharacters) { // if yes
+      console.log(choiceArray = choiceArray.concat(specialCharactersArray)); // adds special characters to choice array
     }
    }
 
    for (i = 0; i < passwordLength; i++) {
     var randomLetter = Math.floor(Math.random() * choiceArray.length);
-    password = password + choiceArray[randomLetter];
+    var password = password + choiceArray[randomLetter];
    }
 
-  return password; // store password as a variable and then call variable
+  return password; // store password as a variable
 }
 
